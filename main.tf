e67518ed8b65efe3645fdcffc1ebabdf97546302
Kanheiya Mishra <50107209+KanheiyaMishra@users.noreply.github.com>
2024-02-28 00:41:06 +0530

    code added to add  new resource group and new virtual network

--- main.tf
+++ main.tf
@@ -16,3 +16,22 @@ provider "azurerm" {
     
   }
 }
+
+
+# Resource Group
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "East US"
+}
+
+# Virtual Network
+resource "azurerm_virtual_network" "example" {
+  name                = "example-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  tags = {
+    environment = "production"
+  }
+}
