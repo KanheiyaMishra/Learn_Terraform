7f4063f6417dc051cb7722490871abe1a0721018
Kanheiya Mishra <50107209+KanheiyaMishra@users.noreply.github.com>
2024-02-28 12:47:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,31 +1,48 @@
-terraform {
-    required_providers {
-      azurerm ={
-        source = "hashicorp/azurerm"
-        version = "3.93.0"
-      }
-    }
-}
-
-provider "azurerm" {
-  features {
-      }
-}
-
-# Resource Group
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
+resource "azurerm_resource_group" "rg" {
+  name     = "myResourceGroup"
   location = "East US"
 }
 
-# Virtual Network
-resource "azurerm_virtual_network" "example" {
-  name                = "example-vnet"
+resource "azurerm_virtual_network" "vnet" {
+  name                = "myVNet"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
 
-  tags = {
-    environment = "production"
+resource "azurerm_subnet" "subnet" {
+  name                 = "mySubnet"
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_public_ip" "public_ip" {
+  name                = "myPublicIP"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+}
+
+# Add more resources as needed (e.g., network security group, network interface, etc.)
+
+resource "azurerm_windows_virtual_machine" "vm" {
+  name                  = "myVM"
+  resource_group_name   = azurerm_resource_group.rg.name
+  location              = azurerm_resource_group.rg.location
+  size                  = "Standard_DS1_v2"
+  admin_username        = "myadminuser"
+  admin_password        = "MyP@ssw0rd!"
+  network_interface_ids = [azurerm_network_interface.nic.id]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2019-Datacenter"
+    version   = "latest"
   }
 }
