911e39a91d8b67b271c2d9e900dcf34869b3a936
Kanheiya Mishra <50107209+KanheiyaMishra@users.noreply.github.com>
2024-02-28 16:03:16 +0530

    Update Providers.tf

--- Providers.tf
+++ Providers.tf
@@ -13,9 +13,9 @@ terraform {
 }
 
 provider "azurerm" {
- #subscription_id = "7b4aaa8b-8ed7-4b46-a50c-32750c8786e7"
-  #client_id = "5085fc2c-ec10-415d-89b6-312b67f7dcdc"
-  #client_secret = "zLF8Q~qcgxSoMztORcogc7zDVuJfzS978fzImcAc"
-  #tenant_id = "c77044d7-ba76-40e3-a712-385bb6d8c277"
+ subscription_id = "7b4aaa8b-8ed7-4b46-a50c-32750c8786e7"
+  client_id = "5085fc2c-ec10-415d-89b6-312b67f7dcdc"
+  client_secret = "zLF8Q~qcgxSoMztORcogc7zDVuJfzS978fzImcAc"
+  tenant_id = "c77044d7-ba76-40e3-a712-385bb6d8c277"
   features {}
 }
