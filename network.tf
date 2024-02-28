f2369414ac87906e760d22b62280b46955e56439
Kanheiya Mishra <50107209+KanheiyaMishra@users.noreply.github.com>
2024-02-28 12:51:48 +0530

    Create network.tf
    created network.tf file

---
+++ network.tf
@@ -0,0 +1,11 @@
+resource "azurerm_network_interface" "nic" {
+  name                = "myNIC"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "myNICConfig"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
